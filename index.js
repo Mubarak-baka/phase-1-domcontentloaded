f8b05a590176c773f49856b7cee3ff302a8e4f5d
Mubarak-baka <Nassibmubarack@gmail.com>
2024-10-07 00:06:23 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code goes here
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
+  
+  
+  
+  
+  document.addEventListener( "DOMContentLoaded", function () {
+      updateDOM()
+    } );
+    
+    
+    function updateDOM() {
+      document.getElementById( "text" )
+        .innerHTML = "This is really cool!";
+    }
